22b4f4fffcef4b4ba2adba012ef9b24b77b35e48
Kpatkar <kiran.patkar@shyrohealtrh.com>
2025-11-09 12:54:32 +0530

    episode-08

---
+++ episode-05.js
@@ -0,0 +1,12 @@
+// require('./path')
+// All the code of the module wrapped inside a function
+// And that function we call it as IIFE - Immediately Invoked Function Expression
+
+// interview question
+
+
+// How are variables and functions private in different module 
+// Because of IIFE and require statement, require statement wrap the code inside the IIFE
+
+// How do you get access to module.exports?
+// Nodejs passess module as a parameter to the IIFE
---
+++ episode-08.js
@@ -0,0 +1,25 @@
+// AST -> Abstract Syntax Tree
+
+// Notes
+// 🔹 1. What is AST?
+
+// AST means Abstract Syntax Tree.
+
+// It shows how your code is structured in a tree form.
+
+// Each part of the code (like variable, loop, function) becomes a node in the tree.
+
+// 🔹 2. Why do we need it?
+
+// Computers don’t understand plain code directly.
+
+// They first convert code → AST → machine code.
+
+// Helps tools to read, check, or modify your code safely.
+
+// 🔹 3. Where is AST used?
+
+// ✅ Node.js (V8 Engine): Converts JS code into AST before running.
+// ✅ Babel: Converts modern JS → older JS using AST.
+// ✅ ESLint: Checks code errors using AST.
+// ✅ Prettier: Formats code by reading AST.
